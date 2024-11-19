577a7d24ed919c896cabf85bc44ba3c146039b1f
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2024-11-18 23:29:56 -0600

    add: package responsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyPortfolioApp());
@@ -23,6 +24,15 @@ class MyPortfolioApp extends StatelessWidget {
         ),
       ),
       themeMode: ThemeMode.system,
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
       home: const PortfolioPage(),
     );
   }
@@ -32,7 +42,7 @@ class PortfolioPage extends StatefulWidget {
   const PortfolioPage({super.key});
 
   @override
-  _PortfolioPageState createState() => _PortfolioPageState();
+  State<PortfolioPage> createState() => _PortfolioPageState();
 }
 
 class _PortfolioPageState extends State<PortfolioPage> {
